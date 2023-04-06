9082a6590e85fedee5e9b69ee09cfd5ed5288b91
FrancieleAires <f.aires2016@gmail.com>
2023-04-06 10:43:15 -0300

    Adicionar arquivos de projeto.

---
+++ Vetor3/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Globalization;
+
+namespace Vetor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N;
+
+            int[] A;
+            int[] B;
+            int[] C;
+            N = int.Parse(Console.ReadLine());
+            A = new int[N];
+            B = new int[N];
+            C = new int[N];
+
+            string[] s = Console.ReadLine().Split(' ');
+            for (int i = 0; i < N; i++)
+            {
+                A[i] = int.Parse(s[i]);
+            }
+
+            s = Console.ReadLine().Split(' ');
+            for (int i = 0; i < N; i++)
+            {
+                B[i] = int.Parse(s[i]);
+            }
+           
+            for (int i = 0; i < N; i++)
+            {
+                C[i] = A[i] + B[i];
+            }
+            for (int i =0; i < N; i++)
+            {
+
+               
+                Console.WriteLine(C[i] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
